6d24a1e27f531cde6fc90b5fe4bb592696469038
Mahan Ziyari <maziyar.mz.021@gmail.com>
2022-01-01 21:28:41 +0330

    Implemented Input Field for Bill Amount

--- JetTipApp/app/src/main/java/com/mahan/jettipapp/MainActivity.kt
+++ JetTipApp/app/src/main/java/com/mahan/jettipapp/MainActivity.kt
@@ -1,32 +1,50 @@
 package com.mahan.jettipapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AttachMoney
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mahan.jettipapp.ui.components.InputField
 import com.mahan.jettipapp.ui.components.TopHeader
 import com.mahan.jettipapp.ui.theme.JetTipAppTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    TopHeader()
+                    Spacer(modifier = Modifier.height(4.dp))
+                    MainContent()
+                }
             }
         }
     }
@@ -42,9 +60,28 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
+@ExperimentalComposeUiApi
 @Preview
 @Composable
 fun MainContent() {
+    BillForm() {
+        Log.d("value", "MainContent: $it")
+    }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+private fun BillForm(
+    modifier: Modifier = Modifier,
+    onValueChanged: (String) -> Unit
+) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(key1 = totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyBoardController = LocalSoftwareKeyboardController.current
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -57,16 +94,22 @@ fun MainContent() {
     ) {
         Column(
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.Start
         ) {
-            Text(
-                text = "Total Per Person",
-                style = MaterialTheme.typography.h5
-            )
+            InputField(
+                modifier = Modifier.fillMaxWidth(),
+                valueState = totalBillState,
+                label = "Bill Amount",
+                leadingIcon = Icons.Rounded.AttachMoney,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
 
-            Text(
-                text = "Total Per Person",
-                style = MaterialTheme.typography.h5
+                    //onValueChanged
+                    onValueChanged(totalBillState.value)
+
+                    keyBoardController!!.hide()
+
+                }
             )
         }
     }
---
+++ JetTipApp/app/src/main/java/com/mahan/jettipapp/ui/components/BillInputField.kt
@@ -0,0 +1,54 @@
+package com.mahan.jettipapp.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    label: String,
+    leadingIcon: ImageVector,
+    isSingleLine: Boolean = true,
+    enabled: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Number,
+    imeAction: ImeAction = ImeAction.Done,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = {valueState.value = it},
+        modifier = modifier.padding(8.dp),
+        label = {
+            Text(text = label)
+        },
+        leadingIcon = {
+            Icon(imageVector = leadingIcon, contentDescription = "Money")
+        },
+        singleLine = isSingleLine,
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            imeAction = imeAction
+        ),
+        textStyle = TextStyle(
+            fontSize = 18.sp,
+            color = MaterialTheme.colors.onBackground
+        ),
+        keyboardActions = onAction
+    )
+}
