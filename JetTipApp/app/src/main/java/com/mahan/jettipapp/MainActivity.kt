32e2c922a3ddff65f82d356f690fab775c960f21
Mahan Ziyari <maziyar.mz.021@gmail.com>
2022-01-01 19:59:55 +0330

    Added Material Icons Dependency

--- JetTipApp/app/src/main/java/com/mahan/jettipapp/MainActivity.kt
+++ JetTipApp/app/src/main/java/com/mahan/jettipapp/MainActivity.kt
@@ -3,11 +3,21 @@ package com.mahan.jettipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mahan.jettipapp.ui.components.TopHeader
 import com.mahan.jettipapp.ui.theme.JetTipAppTheme
 
@@ -28,6 +38,36 @@ fun MyApp(content: @Composable () -> Unit) {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
             content()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainContent() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        border = BorderStroke(
+            width = 1.dp,
+            color = Color.LightGray
+        ),
+        shape = RoundedCornerShape(4.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5
+            )
+
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5
+            )
         }
     }
 }
